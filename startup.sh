f06445651f26a0abf02229428a39692c0458d742
alienmind <cardillo.davide85@gmail.com>
2019-04-06 18:09:18 +0200

    When newest user logs directly to working directiory

--- startup.sh
+++ startup.sh
@@ -7,12 +7,25 @@ echo "Set current user"
 echo "----------------"
 
 USER_ID=$(stat -c '%u' .)
-#USER_NAME=$(stat -c '%U' .)
 GROUP_ID=$(stat -c '%g' .)
 
-echo --- $USER_NAME
+# Save current directory. It is working one.
+WD=$(pwd)
+
 groupadd -g $GROUP_ID $USER_NAME
-useradd -g $GROUP_ID  -m -s /bin/bash -u $USER_ID $USER_NAME
+#if [ -e /home/$USER_NAME ]; then
+if [ -e $WD ]; then
+	# Since scope is to have the same host user profile, should be not need
+	# to create the user's home directory since is should be already to exist
+	useradd -g $GROUP_ID  -d $WD -s /bin/bash -u $USER_ID $USER_NAME
+else
+	# exception use case
+	useradd -g $GROUP_ID  -d $WD -m -s /bin/bash -u $USER_ID $USER_NAME
+fi
+
+# Login as newest user (not as root)
 su - $USER_NAME
+
+
 $@
 
